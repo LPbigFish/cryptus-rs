c86465a7bd20ae3630c0ce097086860e286885b0
LPbigFish <lpbyblock@gmail.com>
2023-04-21 13:35:30 +0200

    Database Logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,63 @@
-use bitcoin::{secp256k1::{Secp256k1, rand}, Address, Network, PublicKey, PrivateKey};
+use std::{fs::{File, self}, io::{Write, Read}};
 
-fn main() {
+use bitcoin::{secp256k1::{Secp256k1, rand}, Address, Network, PublicKey, PrivateKey};
+use flate2::read::GzDecoder;
+use futures_util::StreamExt;
+use reqwest::{self, Client};
+
+
+#[tokio::main]
+async fn main() {
     let wallet = Wallet::new();
 
+    download_database().await;
+
+
     println!("{:#?}", wallet);
+}
+
+async fn download_database() {
+    File::open("database.txt").is_ok().then(|| {
+        fs::remove_file("database.txt").expect("Failed to remove old database file");
+    });
+    File::open("database.txt.gz").is_ok().then(|| {
+        fs::remove_file("database.txt.gz").expect("Failed to remove old database file");
+    });
+    println!("Downloading database...");
+    let res = Client::new()
+        .get("http://addresses.loyce.club/Bitcoin_addresses_LATEST.txt.gz")
+        .send()
+        .await
+        .expect("Failed to download database, Network error");
+    println!("Downloaded database. Size: {:?}", res.content_length().ok_or("Failed to get database size"));
+    
+    File::open("database.txt.gz").is_ok().then(|| {
+        fs::remove_file("database.txt.gz").expect("Failed to remove old database file");
+    });
+
+    let mut file = File::create("database.txt.gz").expect("Failed to create database file");
+
+    let mut stream = res.bytes_stream();
+
+    println!("Writing database...");
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.expect("Failed to download database, Network error");
+        file.write_all(&chunk).expect("Failed to write to database file");
+    }
+    println!("Database is downloaded and written to a file");
+
+    extract_database();
+}
+
+fn extract_database() {
+    let file = File::open("database.txt.gz").expect("Failed to open database file");
+    let mut decoder = GzDecoder::new(file);
+    let mut buf = String::new();
+    decoder.read_to_string(&mut buf).expect("Failed to read from database file");
+    fs::write("database.txt", buf).expect("Failed to write to database file");
+    fs::remove_file("database.txt.gz").expect("Failed to remove old database file");
+    println!("Database is extracted");
 }
 
 #[derive(Debug, Clone)]
