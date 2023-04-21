07b8e48a536b49b7056cb9d978d93d286ef81e98
LPbigFish <82438197+LPbigFish@users.noreply.github.com>
2023-04-21 21:52:39 +0200

    Searchengine beta

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use std::{fs::{File, self}, io::{Write, Read}};
+use std::{fs::{File, self}, io::{Write, self, BufRead}, time::Instant};
 
-use bitcoin::{secp256k1::{Secp256k1, rand}, Address, Network, PublicKey, PrivateKey};
+use bitcoin::{secp256k1::{Secp256k1, rand}, Address, Network, PublicKey, PrivateKey, base58};
 use flate2::read::GzDecoder;
 use futures_util::StreamExt;
+use num::BigUint;
 use reqwest::{self, Client};
+use humansize::{FileSize, file_size_opts};
 
 
 #[tokio::main]
 async fn main() {
     let wallet = Wallet::new();
 
-    download_database().await;
+    //download_database().await;
 
+    println!("{}", wallet.to_string());
 
-    println!("{:#?}", wallet);
+    search_in_db(wallet.address);
 }
 
 async fn download_database() {
@@ -29,7 +32,7 @@ async fn download_database() {
         .send()
         .await
         .expect("Failed to download database, Network error");
-    println!("Downloaded database. Size: {:?}", res.content_length().ok_or("Failed to get database size"));
+    println!("Downloaded database. Size: {:?} Bytes", res.content_length().unwrap().file_size(file_size_opts::BINARY).unwrap());
     
     File::open("database.txt.gz").is_ok().then(|| {
         fs::remove_file("database.txt.gz").expect("Failed to remove old database file");
@@ -47,17 +50,35 @@ async fn download_database() {
     }
     println!("Database is downloaded and written to a file");
 
-    extract_database();
-}
-
-fn extract_database() {
+    
     let file = File::open("database.txt.gz").expect("Failed to open database file");
     let mut decoder = GzDecoder::new(file);
-    let mut buf = String::new();
-    decoder.read_to_string(&mut buf).expect("Failed to read from database file");
-    fs::write("database.txt", buf).expect("Failed to write to database file");
+    io::copy(&mut decoder, &mut File::create("database.txt").expect("Failed to create database file")).expect("Failed to extract database file");
     fs::remove_file("database.txt.gz").expect("Failed to remove old database file");
     println!("Database is extracted");
+
+}
+
+fn search_in_db(address: String) -> bool {
+    let current_time = Instant::now();
+    let searched_address = BigUint::from_bytes_be(&base58::decode(address.to_string().as_str()).unwrap());
+    let file = File::open("database.txt").unwrap();
+    let reader = io::BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let line = BigUint::from_bytes_be(&base58::decode(line.as_str()).unwrap());
+        if line == searched_address {
+            println!("Found address in database");
+            return true;
+        } else if line > searched_address {
+            println!("Address not found in database");
+            println!("Time taken: {}ms", current_time.elapsed().as_millis());
+            return false;
+        }
+    }
+    println!("Address not found in database");
+    println!("Time taken: {}ms", current_time.elapsed().as_millis());
+    false
 }
 
 #[derive(Debug, Clone)]
@@ -72,10 +93,18 @@ impl Wallet {
         let secp = Secp256k1::new();
         let (secret_key, public_key) = secp.generate_keypair(&mut rand::thread_rng());
         let address = Address::p2pkh(&PublicKey::new(public_key), Network::Bitcoin);
+        println!("{:?}", base58::decode(address.to_string().as_str()).unwrap());
         Wallet {
             private_key: PrivateKey::new(secret_key, Network::Bitcoin).to_string(),
             public_key: PublicKey::new(public_key).to_string(),
             address: address.to_string(),
         }
+    }
+}
+
+impl ToString for Wallet {
+    fn to_string(&self) -> String {
+        //convert to json
+        format!("{{\n\t\"private_key\": \"{}\",\n\t\"public_key\": \"{}\",\n\t\"address\": \"{}\"\n}}", self.private_key, self.public_key, self.address)
     }
 }
