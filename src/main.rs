4b1a633891d1e88f478b074460e5990f80995520
LPbigFish <lpbyblock@gmail.com>
2023-04-21 11:44:25 +0200

    Address Generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bitcoin::{secp256k1::{Secp256k1, rand}, Address, Network, PublicKey, PrivateKey};
+
 fn main() {
-    println!("Hello, world!");
+    let wallet = Wallet::new();
+
+    println!("{:#?}", wallet);
+}
+
+#[derive(Debug, Clone)]
+struct Wallet {
+    private_key: String,
+    public_key: String,
+    address: String,
+}
+
+impl Wallet {
+    fn new() -> Self {
+        let secp = Secp256k1::new();
+        let (secret_key, public_key) = secp.generate_keypair(&mut rand::thread_rng());
+        let address = Address::p2pkh(&PublicKey::new(public_key), Network::Bitcoin);
+        Wallet {
+            private_key: PrivateKey::new(secret_key, Network::Bitcoin).to_string(),
+            public_key: PublicKey::new(public_key).to_string(),
+            address: address.to_string(),
+        }
+    }
 }
