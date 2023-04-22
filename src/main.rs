651666ab6083a517397f633be24a21439578f54b
LPbigFish <82438197+LPbigFish@users.noreply.github.com>
2023-04-22 22:40:40 +0200

    Working search engine

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::{File, self}, io::{Write, self, BufRead}, time::Instant};
+use std::{fs::{File, self}, io::{Write, self, BufRead}, time};
 
 use bitcoin::{secp256k1::{Secp256k1, rand}, Address, Network, PublicKey, PrivateKey, base58};
 use flate2::read::GzDecoder;
@@ -12,11 +12,18 @@ use humansize::{FileSize, file_size_opts};
 async fn main() {
     let wallet = Wallet::new();
 
+    let file_list : Vec<String> = fs::read_dir("database").unwrap().map(|res| res.unwrap().file_name().to_str().unwrap().to_string()).collect();
+    //delete .txt from all Strings in file_list
+    let file_names = file_list.iter().map(|s| s.replace(".txt", "")).collect::<Vec<_>>();
+    let file_nums = file_names.iter().map(|s| BigUint::from_bytes_be(&base58::decode(s.as_str()).unwrap().as_slice())).collect::<Vec<_>>();
+
     //download_database().await;
+
+    //split_the_db();
 
     println!("{}", wallet.to_string());
 
-    search_in_db(wallet.address);
+    println!("{}", search_in_db(wallet.address, file_list, file_nums));
 }
 
 async fn download_database() {
@@ -25,6 +32,9 @@ async fn download_database() {
     });
     File::open("database.txt.gz").is_ok().then(|| {
         fs::remove_file("database.txt.gz").expect("Failed to remove old database file");
+    });
+    fs::remove_dir_all("database").is_err().then(|| {
+        fs::create_dir("database").unwrap();
     });
     println!("Downloading database...");
     let res = Client::new()
@@ -57,27 +67,49 @@ async fn download_database() {
     fs::remove_file("database.txt.gz").expect("Failed to remove old database file");
     println!("Database is extracted");
 
+    split_the_db();
 }
 
-fn search_in_db(address: String) -> bool {
-    let current_time = Instant::now();
-    let searched_address = BigUint::from_bytes_be(&base58::decode(address.to_string().as_str()).unwrap());
+fn split_the_db() {
+    println!("Splitting the database...");
+    fs::create_dir("database").is_err().then(|| {
+        fs::remove_dir_all("database").unwrap();
+        fs::create_dir("database").unwrap();
+    });
+
     let file = File::open("database.txt").unwrap();
     let reader = io::BufReader::new(file);
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let line = BigUint::from_bytes_be(&base58::decode(line.as_str()).unwrap());
-        if line == searched_address {
-            println!("Found address in database");
-            return true;
-        } else if line > searched_address {
-            println!("Address not found in database");
-            println!("Time taken: {}ms", current_time.elapsed().as_millis());
-            return false;
+    let local_lines = reader.lines().filter(|line| line.as_ref().unwrap().len() < 35).collect::<Vec<_>>();
+
+    for i in 0..100 {
+        let u = local_lines[(i * (local_lines.len() / 100))..((i + 1) * (local_lines.len() / 100))].iter().peekable().peek().unwrap().as_ref().unwrap();
+        let mut new_file = File::create(format!("database/{}.txt", u)).unwrap();
+
+        for line in local_lines[(i * (local_lines.len() / 100))..((i + 1) * (local_lines.len() / 100))].iter() {
+            let line = line.as_ref().unwrap();
+            new_file.write_all(format!("{}\n", line.to_owned()).as_bytes()).unwrap();
         }
     }
-    println!("Address not found in database");
-    println!("Time taken: {}ms", current_time.elapsed().as_millis());
+}
+
+fn search_in_db(address: String, file_list: Vec<String>, file_nums: Vec<BigUint>) -> bool {
+    let time = time::Instant::now();
+    
+    let address_num = BigUint::from_bytes_be(&base58::decode(&address.as_str()).unwrap().as_slice());
+    
+    for i in 1..file_nums.len() {
+        if file_nums[i - 1] < address_num && file_nums[i] > address_num {
+            let file = File::open(format!("database\\{}", file_list[i - 1])).unwrap();
+            let reader = io::BufReader::new(file);
+            for line in reader.lines() {
+                if line.as_ref().unwrap() == &address {
+                    println!("Time: {:?}", time.elapsed());
+                    return true;
+                }
+            }
+        }
+    }
+    println!("Time: {:?}", time.elapsed());
     false
 }
 
@@ -93,7 +125,6 @@ impl Wallet {
         let secp = Secp256k1::new();
         let (secret_key, public_key) = secp.generate_keypair(&mut rand::thread_rng());
         let address = Address::p2pkh(&PublicKey::new(public_key), Network::Bitcoin);
-        println!("{:?}", base58::decode(address.to_string().as_str()).unwrap());
         Wallet {
             private_key: PrivateKey::new(secret_key, Network::Bitcoin).to_string(),
             public_key: PublicKey::new(public_key).to_string(),
